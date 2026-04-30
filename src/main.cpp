4d0c50f38ab152298bc437df13d62635b7af6f79
gemrey13 <gemreyranola@gmail.com>
2026-04-30 18:21:58 +0800

    Update usage message in main function for clarity; enhance argument count documentation

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,15 @@
 int main(int argc, const char* argv[]) {
     vibeCompiler::InterpreterDriver driver;
 
+    /*
+    argc: This is the "argument count."
+        argc = 1: You just ran ./cook (no file).
+        argc = 2: You ran ./cook script.yow (one file).
+        argc > 2: You tried to run ./cook script.yow extra_file.txt.
+    */
     if (argc > 2) {
-        std::cout << "Usage: ./cook [filename.gz] or just ./cook for interpreter" << std::endl;
+        std::cout << "Too many files! 💀" << std::endl;
+        std::cout << "Usage: ./cook [filename.yow] or just ./cook to start cookin'" << std::endl;
         return 64;
     }
 
